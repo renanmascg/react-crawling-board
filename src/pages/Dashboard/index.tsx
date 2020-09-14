cc1d74f47c31bba282a8a3c458bd541ebdc42b65
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-09-14 15:58:11 -0300

    Adiciona cores diferentes as informações presentes na tela do dashboard, ainda não funcional

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FiDatabase } from 'react-icons/fi';
+import { FaUsers } from 'react-icons/fa';
 import {
   Container,
   HomeContent,
   Header,
   GroupInfo,
   NewestResearch,
+  Title,
+  Info,
 } from './styles';
 import Sidenav from '../../components/sidenav';
 
@@ -12,8 +16,45 @@ const Dashboard: React.FC = () => (
   <Container>
     <Sidenav />
     <HomeContent>
-      <Header />
-      <GroupInfo />
+      <Header>
+        <Title>
+          <h1>Olá, Letícia!</h1>
+          <p>Algumas Estatísticas do mês</p>
+        </Title>
+      </Header>
+      <GroupInfo>
+        <Info>
+          <p>Suas Pesquisas</p>
+          <div>
+            <p>1500</p>
+            <FiDatabase />
+          </div>
+        </Info>
+
+        <Info blue>
+          <p>Total Consultas</p>
+          <div>
+            <p>10</p>
+            <FiDatabase />
+          </div>
+        </Info>
+
+        <Info cyan>
+          <p>Consultas Remanescentes</p>
+          <div>
+            <p>10</p>
+            <FiDatabase />
+          </div>
+        </Info>
+
+        <Info green>
+          <p>Total Usuários</p>
+          <div>
+            <p>10</p>
+            <FaUsers />
+          </div>
+        </Info>
+      </GroupInfo>
       <NewestResearch />
     </HomeContent>
   </Container>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface InfoProps {
+  blue?: boolean;
+  brown?: boolean;
+  cyan?: boolean;
+  green?: boolean;
+}
 
 export const Container = styled.div`
   height: 100vh;
@@ -6,7 +13,134 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const HomeContent = styled.div``;
-export const Header = styled.div``;
-export const GroupInfo = styled.div``;
+export const HomeContent = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: block;
+
+  padding: 30px;
+`;
+
+export const Header = styled.div`
+  height: 80px;
+`;
+
+export const Title = styled.div`
+  color: #26334a;
+
+  h1 {
+    font-size: 25px;
+    font-weight: bold;
+    line-height: 48px;
+  }
+
+  p {
+    margin-top: -15px;
+    font-size: 16px;
+    font-weight: 500;
+    color: #716b6b;
+    line-height: 33px;
+  }
+`;
+
+export const GroupInfo = styled.div`
+  display: flex;
+  height: 150px;
+  justify-content: space-between;
+  width: 100%;
+  flex-wrap: wrap;
+`;
+
+export const Info = styled.div<InfoProps>`
+  width: 250px;
+  background: #ffd5a9;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  margin-top: 10px;
+
+  ${props =>
+    props.blue &&
+    css`
+      background: #c9deff;
+    `}
+
+  ${props =>
+    props.cyan &&
+    css`
+      background: #fdc0c3;
+    `}
+
+  ${props =>
+    props.green &&
+    css`
+      background: #c4efb5;
+    `}
+
+  > p {
+    padding-top: 9px;
+    width: 100%;
+    text-align: center;
+    height: 40px;
+    background: #c7925a;
+    color: #fff;
+    font-weight: 500;
+    ${props =>
+      props.blue &&
+      css`
+        background: #7da3de;
+      `}
+
+    ${props =>
+      props.cyan &&
+      css`
+        background: #bb565b;
+      `}
+
+    ${props =>
+      props.green &&
+      css`
+        background: #84c46e;
+      `}
+  }
+
+  div {
+    margin: 20px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+      font-weight: bold;
+      font-size: 40px;
+      color: #80490f;
+      flex: 1;
+      ${props =>
+        props.blue &&
+        css`
+          color: #002661;
+        `}
+
+      ${props =>
+        props.cyan &&
+        css`
+          color: #640005;
+        `}
+
+      ${props =>
+        props.green &&
+        css`
+          color: #1a6500;
+        `}
+    }
+
+    svg {
+      width: 50px;
+      height: 50px;
+      color: #000000;
+    }
+  }
+`;
+
 export const NewestResearch = styled.div``;
