963daf6b24d2f529f76d9c6a5cef6ef23ad70bd4
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-09-22 04:27:14 -0300

    Adiciona graficos na tela de resultados descriminados da pesquisa

--- src/pages/ResultQuery/index.tsx
+++ src/pages/ResultQuery/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
-import { Container, Content } from './styles';
+import { Pie, ChartComponentProps } from 'react-chartjs-2';
+import {
+  Container,
+  Content,
+  ChartsInfo,
+  ChartstTitle,
+  ChartsBody,
+  NumberInfo,
+} from './styles';
 import Sidenav from '../../components/sidenav';
 
 interface IParams {
@@ -10,10 +18,67 @@ interface IParams {
 const ResultQuery: React.FC = () => {
   const { id } = useRouteMatch<IParams>().params;
   console.log(`=================> ${id}`);
+
+  const dataset = {
+    datasets: [
+      {
+        data: [15, 20],
+        backgroundColor: ['#36A2EB', '#FFCE56', '#36A2EB'],
+        // hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        borderColor: '#F4F6F9',
+      },
+    ],
+    labels: ['Avaliados', 'Não Avaliados'],
+  };
+
+  const data: ChartComponentProps = {
+    data: dataset,
+    legend: {
+      display: true,
+      position: 'top',
+      fullWidth: true,
+      reverse: false,
+      labels: {
+        fontColor: '#3c3c3c',
+        fontFamily: 'Poppins',
+      },
+    },
+    options: {
+      maintainAspectRatio: false,
+    },
+  };
+
   return (
     <Container>
       <Sidenav />
-      <Content />
+      <Content>
+        <ChartsInfo>
+          <ChartstTitle>
+            <h1>Estatísticas da Pesquisa</h1>
+            <hr />
+          </ChartstTitle>
+          <ChartsBody>
+            <NumberInfo>
+              <p>Número de resultados:</p>
+              <span>
+                <h1>1234 Resultados</h1>
+              </span>
+            </NumberInfo>
+            <div>
+              <Pie {...data} redraw />
+            </div>
+            <div>
+              <Pie {...data} />
+            </div>
+            <div>
+              <Pie {...data} />
+            </div>
+            <div>
+              <Pie {...data} />
+            </div>
+          </ChartsBody>
+        </ChartsInfo>
+      </Content>
     </Container>
   );
 };
--- src/pages/ResultQuery/styles.ts
+++ src/pages/ResultQuery/styles.ts
@@ -10,12 +10,84 @@ export const Container = styled.div`
 export const Content = styled.div`
   flex: 1;
   overflow: auto;
-  padding: 20px;
   display: flex;
   flex-direction: column;
 
   hr {
     margin: 20px 0;
     border: 1px solid #cadfff;
+  }
+`;
+
+export const ChartsInfo = styled.div`
+  height: 300px;
+  background: #6078eb;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const ChartstTitle = styled.div`
+  display: flex;
+  padding: 5px;
+  border-radius: 5px;
+  align-items: center;
+
+  h1 {
+    margin-right: 10px;
+    color: white;
+    font-size: 22px;
+  }
+
+  hr {
+    flex: 1;
+    margin: 20px 0;
+    border: 1px solid #cadfff;
+    height: 1px;
+    background: #cadfff;
+  }
+`;
+
+export const ChartsBody = styled.div`
+  flex: 1;
+  padding-top: 5px;
+  display: flex;
+  overflow: auto;
+
+  div {
+    background: white;
+    width: 300px;
+    height: 100%;
+    border-radius: 10px;
+    padding: 15px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+
+    & + div {
+      margin-left: 15px;
+    }
+  }
+`;
+
+export const NumberInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px !important;
+
+  p {
+    width: 270px;
+    font-size: 16px;
+    font-weight: normal;
+  }
+
+  span {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 30px;
   }
 `;
