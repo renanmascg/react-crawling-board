cebd723f6c075e98d1572ddc06d70b84093fa282
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-11-09 09:45:08 -0300

    Adiciona endpoint final da aplicação

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import 'dotenv/config';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://crawling-backend.herokuapp.com',
 });
 
 export default api;
